35fb252693861202933283644ed812a827b23a5d
roylines <roy@roylines.co.uk>
2020-02-10 18:29:01 +0000

    implemented for ddb and memory

--- index.js
+++ index.js
@@ -1,14 +1,52 @@
-const LRU = require('lru-cache');
-const cache = new LRU({max: 10});
+const cache = require('./lib/cache');
+const ddb = require('./lib/ddb');
+
+let _config = {
+  tablename: 'idempotency',
+  idField: 'id',
+  ttlField: 'ttl',
+  ttl: 60,
+};
+
+const configure = async config => {
+  _config = {..._config, ...config};
+};
+
+const checkDynamoDB = async ({id}) => {
+  let ttl = Math.floor(Date.now() / 1000) + 60;
+
+  let params = {
+    TableName: _config.tablename,
+    ConditionExpression: `attribute_not_exists(${_config.idField})`,
+    Key: {},
+    Item: {},
+  };
+
+  params.Key[_config.idField] = id.toString();
+  params.Item[_config.idField] = id.toString();
+  params.Item[_config.ttlField] = ttl;
+
+  try {
+    await ddb.put(params);
+    return {replay: false};
+  } catch (e) {
+    if (e.code == 'ConditionalCheckFailedException') {
+      return {replay: true};
+    }
+    throw e;
+  }
+};
 
 const check = async ({id}) => {
   // check the cache
-  const cached = cache.get(id);
-  if (cached) return {replay: true};
+  if (cache.exists(id)) return {replay: true};
 
-  // get the real value
-  cache.set(id, true);
-  return {replay: false};
+  const ret = await checkDynamoDB({id});
+  cache.set(id);
+  return ret;
 };
 
-module.exports = {check};
+module.exports = {
+  configure,
+  check,
+};
--- index.test.js
+++ index.test.js
@@ -1,10 +1,83 @@
-const {check} = require('.');
+const {check, configure} = require('.');
+const {put} = require('./lib/ddb');
+const {exists, set} = require('./lib/cache');
+
+jest.mock('./lib/ddb');
+jest.mock('./lib/cache');
 
 describe('idempotency', () => {
-  it('should block replay', async () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+    global.Date.now = jest.fn(() => 1000);
+  });
+
+  it('should return replay true if cached', async () => {
+    exists.mockReturnValue(true);
+    const {replay} = await check({id: 1});
+    expect(replay).toEqual(true);
+
+    expect(exists.mock.calls).toMatchSnapshot();
+    expect(set.mock.calls).toMatchSnapshot();
+    expect(put.mock.calls).toMatchSnapshot();
+  });
+
+  it('should return replay true if not cached, but in ddb', async () => {
+    exists.mockReturnValue(false);
+    put.mockRejectedValue({code: 'ConditionalCheckFailedException'});
+
+    const {replay} = await check({id: 1});
+    expect(replay).toEqual(true);
+
+    expect(exists.mock.calls).toMatchSnapshot();
+    expect(set.mock.calls).toMatchSnapshot();
+    expect(put.mock.calls).toMatchSnapshot();
+  });
+
+  it('should return replay false if not cached, or not in ddb', async () => {
+    exists.mockReturnValue(false);
+    put.mockResolvedValue();
+
+    const {replay} = await check({id: 1});
+    expect(replay).toEqual(false);
+
+    expect(exists.mock.calls).toMatchSnapshot();
+    expect(set.mock.calls).toMatchSnapshot();
+    expect(put.mock.calls).toMatchSnapshot();
+  });
+
+  it('should allow things to be configured', async () => {
+    config = {
+      tablename: 'TABLE',
+      idField: 'ID',
+      ttlField: 'TTL',
+      ttl: 42,
+    };
+
+    exists.mockReturnValue(false);
+    put.mockResolvedValue();
+
+    await configure(config);
+    await check({id: 1});
+
+    expect(put.mock.calls).toMatchSnapshot();
+  });
+
+  /*
+  it.skip('can configure', async () => {
+    await configure({tablename: 'XX'});
+    // TODO assert DDB calls
+  });
+
+  it.skip('should return {replay} correctly', async () => {
     const first = await check({id: 1});
     expect(first.replay).toBe(false);
     const second = await check({id: 1});
     expect(second.replay).toBe(true);
   });
+
+  it('should call ddb correctly', async () => {
+    const {replay} = await check({id: 3});
+    expect(replay).toEqual(false);
+  });
+  */
 });
